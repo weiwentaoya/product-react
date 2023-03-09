4e035178db4e964a251d5603350522132de35bda
wentao.wei <wentao.wei@asiainnovations.com>
2023-03-09 15:51:16 +0800

    🎉 init:

---
+++ src/layout/default.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { NavBar, Toast } from 'antd-mobile'
+import BottomBar from '../components/core/BottomBar'
+import './default.less'
+
+
+const layout = props => {
+  const back = () =>
+    Toast.show({
+      content: '点击了 返回区域',
+      duration: 1000
+    })
+  return (
+    <div className='page-container__wrap'>
+      <NavBar onBack={back}>layout</NavBar>
+      <div className='page-main__wrap'>
+        {props.children}
+      </div>
+      <BottomBar></BottomBar>
+    </div>
+  )
+}
+
+export default layout
